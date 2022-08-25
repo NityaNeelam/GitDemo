78ec2cef615d8c5d93ccac18cebac716cf07382a
NityaNeelam <nityav30@gmail.com>
2022-08-25 22:27:55 +0530

    first commit

---
+++ src/main/java/qaclickacademy/App.java
@@ -0,0 +1,31 @@
+package qaclickacademy;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App {
+
+	int i=10;
+	int j=20;
+	char c;
+	
+	public void test() {
+		int a=10;
+		int b=30;
+		
+	}
+	
+	public static void main(String[]  args) throws Exception {
+	
+      App obj = new App();
+      System.out.println(obj.i);
+    //  System.out.println(obj.a);
+		
+		
+	}
+}
---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void IOSApps() {
+		
+		System.out.println("IOS APPS");
+	}
+	
+}
